69c31e336c55faf48ef2b7fd0a0faed5f1d7378e
Sanamunir16 <sanamunir1605@gmail.com>
2025-12-06 16:02:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-$(document).ready(function() {
-    // Fade-in animation for heading
+$(document).ready(function () {
+
+    // Fade animation on headings
     $(".fade").addClass("show");
 
+    // Show popup only on Home Page (index.html)
+    if (window.location.pathname.includes("index.html") || window.location.pathname.endsWith("/")) {
+        setTimeout(function () {
+            $("#welcomePopup").addClass("show");
+        }, 800);
+    }
+
+    // Close popup
+    $("#closePopup").click(function () {
+        $("#welcomePopup").removeClass("show");
+    });
+
     // Smooth page transitions
-    $("nav a").click(function(e) {
-        var link = $(this).attr("href");
-        $("body").fadeOut(300, function() {
+    $("nav a").click(function (e) {
+        let link = $(this).attr("href");
+        $("body").fadeOut(300, function () {
             window.location.href = link;
         });
         e.preventDefault();
